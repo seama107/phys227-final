5df6b0578863174ecca568ad149ae50bc644555a
Michael <micseam@yahoo.com>
2016-05-20 16:04:49 -0700

    Created Rossler.gif

--- generate_graphs.py
+++ generate_graphs.py
@@ -10,6 +10,10 @@ Description: For final exam - simply generates the needed graphs
 
 from final import Rossler
 
+import sys
+import subprocess
+import numpy as np
+
 c_values = [2, 3, 4, 4.15, 4.2, 5.7]
 for c in c_values:
     Rc_2 = Rossler(c)
@@ -19,5 +23,20 @@ for c in c_values:
     Rc_2.plotx()
     Rc_2.plotxy()
     Rc_2.plotxyz()
+
+
+
+# For generating a gif
+
+# c_values = np.arange(2, 8, .25)
+# for c in c_values:
+#     Rc_2 = Rossler(c)
+#     print "Running RK4 for c = %f ..." % c
+#     Rc_2.run()
+#     print "Plotting ..."
+#     Rc_2.plotxyz()
+
+# subprocess.call("convert -delay 10 -loop 0 *.png Rossler.gif", shell=True)
+# #subprocess.call("rm *.png", shell=True)
 
 print "Complete!"
