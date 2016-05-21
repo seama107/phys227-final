d37a7f9814e9c4e26a5d82daf7150206dece6654
Michael <micseam@yahoo.com>
2016-05-20 21:07:01 -0700

    Created extra plots

--- generate_scatterplot.py
+++ generate_scatterplot.py
@@ -16,6 +16,8 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from final import findmaxima
 
+
+#Calculating for X
 fig, ax = plt.subplots(nrows = 1, ncols = 1)
 
 c_values = np.arange(2, 6, .01)
@@ -31,4 +33,43 @@ ax.set_ylim((3, 12))
 ax.set_title("Maxima on x vs c")
 ax.grid(True)
 fig.savefig("Scatterplot.png")
+plt.close(fig)
+
+
+# For Y
+fig, ax = plt.subplots(nrows = 1, ncols = 1)
+
+c_values = np.arange(2, 6, .1)
+for c in c_values:
+    sys.stdout.write('.')
+    sys.stdout.flush()
+    maxima = findmaxima(c, var = 'y')
+    for m in maxima:
+        ax.plot(c, m, '.')
+ax.set_xlabel("c")
+ax.set_ylabel("y maxima")
+ax.set_ylim((0, 9))
+ax.set_title("Maxima on y vs c")
+ax.grid(True)
+fig.savefig("Scatterploty.png")
+plt.close(fig)
+print ""
+
+
+# For Z
+fig, ax = plt.subplots(nrows = 1, ncols = 1)
+
+c_values = np.arange(2, 6, .1)
+for c in c_values:
+    sys.stdout.write('.')
+    sys.stdout.flush()
+    maxima = findmaxima(c, var = 'z')
+    for m in maxima:
+        ax.plot(c, m, '.')
+ax.set_xlabel("c")
+ax.set_ylabel("z maxima")
+ax.set_ylim((0, 25))
+ax.set_title("Maxima on z vs c")
+ax.grid(True)
+fig.savefig("Scatterplotz.png")
 plt.close(fig)
